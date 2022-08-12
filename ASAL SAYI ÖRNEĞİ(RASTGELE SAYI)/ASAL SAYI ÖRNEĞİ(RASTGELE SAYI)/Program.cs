97b234b98200b9e545aa807c0f5aa74d7f8bb3d1
yunusyiilmaz <yns.ylmz1381@gmail.com>
2022-08-12 18:13:32 +0300

    İlk 50 asal sayıyı bulan program
    İlk 50 asal sayıyı bulan program

---
+++ ASAL SAYI ÖRNEĞİ(RASTGELE SAYI)/ASAL SAYI ÖRNEĞİ(RASTGELE SAYI)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASAL_SAYI_ÖRNEĞİ_RASTGELE_SAYI_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İLK 50 ASAL SAYI");
+            Console.WriteLine("*******************");
+            Console.WriteLine();
+            int son = 0;
+            int sayi = 0;
+            int kontrol = 0;
+            int sütun = 0;
+            while (true)
+            {
+                if (son == 50)
+                {
+                    break;
+                }
+                kontrol = 0;
+                for (int j = 2; j < sayi; j++)
+                {
+                    if (sayi % j == 0)
+                    {
+                        kontrol++;
+                    }
+                }
+                if (kontrol == 0)
+                {
+                    sütun++;
+                    Console.Write(sayi + " ");
+                    if(sütun%10==0)
+                    {
+                        Console.WriteLine();
+                    }
+                    son++;
+                }
+                sayi++;
+            }
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
